8692ea6990bc228a6aa24d567bde6c822f743ecd
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-02 18:05:31 +0000

    Mar 2, 2026, 7:05 PM

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();   // Load environment variables
+
+connectDB();       // Connect to MongoDB
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Test Route
@@ -15,7 +23,7 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'API is working 🚀' });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
