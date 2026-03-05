b0256b5babb3d83e06f2b62d28aaf49addeb4f71
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-05 12:42:45 +0000

    Mar 5, 2026, 1:42 PM

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,29 +1,16 @@
+// controllers/contactController.js
 const emailService = require("../services/emailService");
 
-exports.contact = async (req, res) => {
-
+exports.submitContact = async (req, res) => {
   try {
-
     const { name, email, phone, message } = req.body;
-
-    const data = { name, email, phone, message };
-
-    await emailService.sendContactEmails(data);
-
+    await emailService.sendContactEmails({ name, email, phone, message });
     res.status(200).json({
       success: true,
       message: "Message sent successfully"
     });
-
   } catch (error) {
-
     console.error(error);
-
-    res.status(500).json({
-      success: false,
-      message: "Server error"
-    });
-
+    res.status(500).json({ success: false, message: "Server error" });
   }
-
 };
