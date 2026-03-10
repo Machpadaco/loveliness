1c3bce6c1ba92bf9c043d193a2374feefd72b02f
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-10 16:01:32 +0000

    Mar 10, 2026, 5:01 PM

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // Routes
 app.use('/api/contact', require('./routes/contactRoutes'));
 app.use('/api/counselling', require('./routes/counsellingRoutes')); // Counselling route added
-app.use("/api/volunteer", require("./routes/volunteerRoutes"));
+app.use('/api/volunteer', require('./routes/volunteerRoutes'));
 
 // Test Route
 app.get('/', (req, res) => {
