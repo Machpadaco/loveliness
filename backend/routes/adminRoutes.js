4e3f2531a784d5371e32401a6e18ac61d9b2182e
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-11 10:53:32 +0000

    Mar 11, 2026, 11:53 AM

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -11,12 +11,20 @@ const {
 } = require("../controllers/adminController");
 
 
+/* =========================
+   GET DATA
+========================= */
+
 router.get("/contacts", getContacts);
 
 router.get("/counselling", getCounselling);
 
 router.get("/volunteers", getVolunteers);
 
+
+/* =========================
+   DELETE DATA
+========================= */
 
 router.delete("/contact/:id", deleteContact);
 
