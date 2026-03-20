06e2cd09edfd36cdbb655192497759b1eb31bc24
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-20 10:30:18 +0000

    Mar 20, 2026, 11:30 AM

--- backend/models/Counselling.js
+++ backend/models/Counselling.js
@@ -3,42 +3,51 @@ const mongoose = require("mongoose");
 const counsellingSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
 
   email: {
     type: String,
-    required: true
+    required: true,
+    trim: true,
+    lowercase: true
   },
 
   phone: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
 
   country: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
 
   counsellingType: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
 
   preferredContact: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
 
   message: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
 
   createdAt: {
     type: Date,
     default: Date.now
   }
-
 });
 
 module.exports = mongoose.model("Counselling", counsellingSchema);
