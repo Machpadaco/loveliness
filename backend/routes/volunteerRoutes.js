aebe5c14935f3f5a7b348643bbe94502ef4cb2d5
EJIKE NJUWA <machpadacoglobalservices@gmail.com>
2026-03-21 08:42:03 +0000

    Mar 21, 2026, 9:42 AM

--- backend/routes/volunteerRoutes.js
+++ backend/routes/volunteerRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { submitVolunteer } = require("../controllers/volunteerController");
 
+// POST: Submit volunteer application
 router.post("/", submitVolunteer);
 
 module.exports = router;
